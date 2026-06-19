f358e54b955e3aae749ab52b240cc9c38363ca30
Tanmay Pawar <tanmaypawar230705@gmail.com>
2026-06-19 15:04:07 +0530

    Position Data added to Database

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,46 @@ const uri = process.env.MONGO_URL;
 
 const app = express();
 
+// app.get("/addPositions", async (req, res) => {
+//   let tempPositions = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
+//   tempPositions.forEach((item) => {
+//     let newPosition = new PositionsModel({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss: item.isLoss,
+//     });
+
+//     newPosition.save();
+//   });
+//   res.send("Done!");
+// });
+
 // app.get("/addHoldings", async (req, res) => {
 //   let tempHoldings = [
 //     {
